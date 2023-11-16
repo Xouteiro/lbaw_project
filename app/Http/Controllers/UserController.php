499ffb4f3dc5ac572d18e5e734f10a158170b0d7
xouteiro <xavierouteiro@gmail.com>
2023-11-16 10:20:17 +0000

    finished user controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,66 +4,71 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create(Request $request)
-    {
-        $user = new User();
-
-        //authorize
-
-        $user->email = $request->input('email');
-        $user->username = $request->input('username');
-        $user->name = $request->input('name');
-        $user->password = $request->input('password');
-
-        $user->save();
-        return redirect('/login');
-        
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
-    public function show(User $user)
+    public function show(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        //authorize->policies
+
+        return view('pages.user', [
+            'user' => $user
+        ]);
+
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User $user)
+    public function edit(Request $request, string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        //authorize edit
+
+        $request->validate([
+            'email' => 'required|email|max:250|unique:users',
+            'username' => 'required|string|max:250|unique:users',
+            'name' => 'required|string|max:250',
+            'description' => 'string|max:250'
+        ]);
+
+        if($request->password){
+            $request->validate([
+                'password' => 'required|min:8|confirmed'
+            ]);
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->email = $request->email;
+        $user->username = $request->username;
+        $user->name = $request->name;
+        $user->description = $request->description;
+
+        $user->save();
+        return redirect()->route('user.show', ['id' => $user->id])
+            ->withSuccess('You have successfully edited your profile!');
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+    public function delete(string $id) //fazer com que os eventos/comentarios/etc nao sejam apagados
     {
-        //
+        $user = User::findOrFail($id);
+        //authorize delete
+        $user->delete();
+        return redirect()->route('home')
+            ->withSuccess('You have successfully deleted your profile!');
+        
     }
 }
