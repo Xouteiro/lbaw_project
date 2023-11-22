abf768089498e91ad2f9775f44ac54a871bc5b52
José Luiz <ZLAmoedo@gmail.com>
2023-11-22 23:47:33 +0000

    Adjustments do HTML and CSS

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -1,16 +1,18 @@
 <li>
+    <div class="comment">
     <h3>{{ $comment->user->username }}</h3>
     @if (Auth::check() && (Auth::id() === $comment->user->id || Auth::user()->admin))
-        <form action="" method="POST">
-            <button class="delete_comment_button" type="submit">
-                <p> Delete comment</p>
-            </button>
-        </form>
         <form action="" method="POST">
             <button class="edit_comment_button" type="submit">
                 <p> Edit comment</p>
             </button>
         </form>
+        <form action="" method="POST">
+            <button class="delete_comment_button" type="submit">
+                <p> Delete comment</p>
+            </button>
+        </form>
     @endif
+    </div>
     <p>{{ $comment->text }}</p>
 </li>
