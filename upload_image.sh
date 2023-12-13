bdf81b943fe9fdeb9d7cc091f912e7d225681064
xouteiro <xavierouteiro@gmail.com>
2023-12-13 22:49:05 +0000

    fixed upload_image

--- upload_image.sh
+++ upload_image.sh
@@ -3,7 +3,7 @@
 # Stop execution if a step fails
 set -e
 
-IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2324/lbaw2354
+IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2223/lbaw2255
 
 # Ensure that dependencies are available
 composer install
@@ -11,5 +11,6 @@ php artisan config:clear
 php artisan clear-compiled
 php artisan optimize
 
-docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
+# docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
