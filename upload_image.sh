2f538de6c3f839ba0789c7687dea7d42f23d40cc
coutinho21 <guilhermecscoutinho@gmail.com>
2023-12-14 09:15:20 +0000

    update image file...

--- upload_image.sh
+++ upload_image.sh
@@ -11,5 +11,5 @@ php artisan config:clear
 php artisan clear-compiled
 php artisan optimize
 
-docker buildx build --push --platform linux/arm64v8 -t $IMAGE_NAME .
+docker buildx build --push --platform arm64v8 -t $IMAGE_NAME .
 docker push $IMAGE_NAME
